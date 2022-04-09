a31dd7ee9c3816c2b6676dd5dacbcf2e545214ae
Tabahi Repository <alabdulrehman@hotmail.fr>
2022-04-09 20:06:08 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,14 +32,14 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate web interface to control your IoT devices'
+      description: 'The ultimate cloud to control your IoT devices'
     }
   },
   metaInfo() {
     return {
       title: this.description,
       meta: [
-        { key: 'description', name: 'description', content: 'A web interface library to easily manage, control, communicate with your IoT devices.' }
+        { key: 'description', name: 'description', content: 'A cloud service to easily manage, control, communicate with your IoT devices using our arduino library.' }
       ]
     }
   }
