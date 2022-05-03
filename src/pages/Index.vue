85c40777cbaa4090a502883c9c10c6b0f8449e08
Tabahi Repository <alabdulrehman@hotmail.fr>
2022-05-03 12:59:38 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,8 +5,8 @@
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/arduino-library" text="Arduino Library" icon="square-icon" />
-        <Shortcut link="/scripts" text="Scripts" icon="square-icon" />
+        <Shortcut link="/arduino-library" text="Arduino Library" icon="cpu-icon" />
+        <Shortcut link="/scripts" text="Scripts" icon="code-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
